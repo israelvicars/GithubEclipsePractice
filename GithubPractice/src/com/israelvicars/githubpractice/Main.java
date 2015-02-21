373bc1ee82be8af9855277cb5fb81bd15e9505ca
Israel Vicars <israelvicars@gmail.com>
2015-02-21 11:13:29 -0500

    Initial setup of repository.

---
+++ GithubPractice/src/com/israelvicars/githubpractice/Main.java
@@ -0,0 +1,13 @@
+package com.israelvicars.githubpractice;
+
+public class Main {
+	static String whatKindaMan = "main";
+	
+	public static void main(String[] args) {
+		System.out.println("My " + whatKindaMan + " man!");
+	}
+
+	public static String myWhatMan () {
+		return whatKindaMan;
+	}
+}
---
+++ GithubPractice/src/com/israelvicars/githubpractice/WhatMan.java
@@ -0,0 +1,9 @@
+package com.israelvicars.githubpractice;
+
+public class WhatMan extends Main {
+	static String aKindaMan = myWhatMan();
+	
+	public static void main (String[] args) {
+		System.out.println("What kinda man? A " + aKindaMan + " man.");
+	}
+}
